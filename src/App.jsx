bbfb72285d9fe21cf0e58c411726bc67e6c39f9c
thxDuck <dylan.pelle.dev@gmail.com>
2022-05-20 16:52:16 +0200

    Init project with CRA, init scss files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+	return (
+		<>
+			<Header />
+			<h1>Hello SportSee !</h1>
+			<Footer />
+		</>
+	);
+}
+
+export default App;
