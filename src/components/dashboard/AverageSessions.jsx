4146c3cb9a6a5b86264d7d75bc953eb70888df19
Dylan Pellé <dylan.pelle.dev@gmail.com>
2022-06-07 13:59:13 +0200

    test rechart

--- src/components/dashboard/AverageSessions.jsx
+++ src/components/dashboard/AverageSessions.jsx
@@ -1,10 +1,120 @@
-import React from 'react'
-
+import React from "react"
+import {LineChart, Line, Tooltip, CartesianGrid, XAxis, YAxis} from "recharts"
+// https://recharts.org/en-US/api/LineChart
+// const USER_AVERAGE_SESSIONS = [
+//     {
+//         userId: 12,
+//         sessions: [
+//             {
+//                 day: 1,
+//                 sessionLength: 30
+//             },
+//             {
+//                 day: 2,
+//                 sessionLength: 23
+//             },
+//             {
+//                 day: 3,
+//                 sessionLength: 45
+//             },
+//             {
+//                 day: 4,
+//                 sessionLength: 50
+//             },
+//             {
+//                 day: 5,
+//                 sessionLength: 0
+//             },
+//             {
+//                 day: 6,
+//                 sessionLength: 0
+//             },
+//             {
+//                 day: 7,
+//                 sessionLength: 60
+//             }
+//         ]
+//     },
+//     {
+//         userId: 18,
+//         sessions: [
+//             {
+//                 day: 1,
+//                 sessionLength: 30
+//             },
+//             {
+//                 day: 2,
+//                 sessionLength: 40
+//             },
+//             {
+//                 day: 3,
+//                 sessionLength: 50
+//             },
+//             {
+//                 day: 4,
+//                 sessionLength: 30
+//             },
+//             {
+//                 day: 5,
+//                 sessionLength: 30
+//             },
+//             {
+//                 day: 6,
+//                 sessionLength: 50
+//             },
+//             {
+//                 day: 7,
+//                 sessionLength: 50
+//             }
+//         ]
+//     }
+// ]
 
 const AverageSessions = () => {
-    return (
-        <div id="averageSessions">AverageSessions</div>
-    )
+	const data = [
+		{
+			name: "L",
+			avg: 30,
+			label: "Foo",
+		},
+		{
+			name: "M",
+			avg: 40,
+		},
+		{
+			name: "M",
+			avg: 50,
+		},
+		{
+			name: "J",
+			avg: 30,
+		},
+		{
+			name: "V",
+			avg: 30,
+		},
+		{
+			name: "S",
+			avg: 50,
+		},
+		{
+			name: "D",
+			avg: 50,
+		},
+	]
+	return (
+		<div id="averageSessions">
+            <LineChart width={600} height={300} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}  data={data} style={{"background-color": "red;"}}>
+				<Line type="natural" dataKey="avg" stroke="#8884d8" strokeWidth="4" />
+				{/* <CartesianGrid stroke="#FF0000" fill="#FF0000" /> */}
+				{/* <XAxis dataKey="name" /> */}
+				<XAxis dataKey="name" />
+				{/* <YAxis /> */}
+				<Tooltip />
+                
+			</LineChart>
+		</div>
+	)
 }
 
-export default AverageSessions;
+export default AverageSessions
--- src/components/dashboard/UserPerformence.jsx
+++ src/components/dashboard/UserPerformence.jsx
@@ -1,9 +1,135 @@
 import React from 'react'
-
-
+import rd3 from 'react-d3-library';
+const RD3Component = rd3.Component;
+const USER_PERFORMANCE = [
+    {
+        userId: 12,
+        kind: {
+            1: 'cardio',
+            2: 'energy',
+            3: 'endurance',
+            4: 'strength',
+            5: 'speed',
+            6: 'intensity'
+        },
+        data: [
+            {
+                value: 80,
+                kind: 1
+            },
+            {
+                value: 120,
+                kind: 2
+            },
+            {
+                value: 140,
+                kind: 3
+            },
+            {
+                value: 50,
+                kind: 4
+            },
+            {
+                value: 200,
+                kind: 5
+            },
+            {
+                value: 90,
+                kind: 6
+            }
+        ]
+    },
+    {
+        userId: 18,
+        kind: {
+            1: 'cardio',
+            2: 'energy',
+            3: 'endurance',
+            4: 'strength',
+            5: 'speed',
+            6: 'intensity'
+        },
+        data: [
+            {
+                value: 200,
+                kind: 1
+            },
+            {
+                value: 240,
+                kind: 2
+            },
+            {
+                value: 80,
+                kind: 3
+            },
+            {
+                value: 80,
+                kind: 4
+            },
+            {
+                value: 220,
+                kind: 5
+            },
+            {
+                value: 110,
+                kind: 6
+            }
+        ]
+    }
+]
 const UserPerformences = () => {
+    const data = {
+        classname: "Perfomence",
+        axes: [
+            { axis: "Cardio", value: 200 },
+            { axis: "Energie", value: 240 },
+            { axis: "Endurance", value: 800 },
+            { axis: "Force", value: 803 },
+            { axis: "Vitesse", value: 220 },
+            { axis: "Intensité", value: 110 }
+        ]
+    }
     return (
-        <div id="userPerformence">UserPerformences</div>
+        <div id="userPerformence">
+            <RD3Component data={data} />
+        </div>
+        // <div id="userPerformence">
+        //     <Radar
+        //         style={{
+        //             width: "100%",
+        //             height: "100%",
+        //         }}
+        //         width={200}
+        //         height={200}
+        //         padding={20}
+        //         domainMax={300}
+        //         highlighted={true}
+        //         data={{
+        //             variables: [
+        //                 { key: "cardio", label: "Cardio" },
+        //                 { key: "energy", label: "Energie" },
+        //                 { key: "endurance", label: "Endurance" },
+        //                 { key: "strength", label: "Force" },
+        //                 { key: "speed", label: "Vitesse" },
+        //                 { key: "intensity", label: "Intensité" }
+        //             ],
+        //             sets: [
+        //                 {
+        //                     key: "me",
+        //                     label: "My Scores",
+        //                     values: {
+        //                         cardio: 200,
+        //                         energy: 240,
+        //                         endurance: 80,
+        //                         strength: 80,
+        //                         speed: 220,
+        //                         intensity: 110
+        //                     }
+        //                 }
+        //             ]
+        //         }}
+        //     />
+        // </div>
     )
 }
 
